d7b06652d3d61d0f26128a2e0e977cf9640033a4
hikikomoriphoenix <dondon1987@yandex.com>
2018-06-28 18:06:36 +0800

    Initial commit

---
+++ app/src/main/java/marabillas/loremar/gamehunter/MainActivity.java
@@ -0,0 +1,13 @@
+package marabillas.loremar.gamehunter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_activity_main);
+    }
+}
