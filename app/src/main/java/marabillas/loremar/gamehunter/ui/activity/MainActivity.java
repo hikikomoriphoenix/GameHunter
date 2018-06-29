7a3e68f9cc3829a6d101aafa41883621084b82e7
hikikomoriphoenix <dondon1987@yandex.com>
2018-06-29 14:39:35 +0800

    Conceptualize initial structure for source files

---
+++ app/src/main/java/marabillas/loremar/gamehunter/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package marabillas.loremar.gamehunter.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import marabillas.loremar.gamehunter.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_activity_main);
+    }
+}
