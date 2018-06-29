ea2580eb6fe468f208e863ec591250f352d92d3c
hikikomoriphoenix <dondon1987@yandex.com>
2018-06-29 17:22:19 +0800

    add and implement JsonParser class

---
+++ app/src/androidTest/java/marabillas/loremar/gamehunter/parsers/JSONParserTest.java
@@ -0,0 +1,47 @@
+package marabillas.loremar.gamehunter.parsers;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class JSONParserTest {
+    @Test
+    public void parse() {
+        JSONObject json = new JSONParser().parse("https://raw.githubusercontent" +
+                ".com/hikikomoriphoenix/android-libraries-lab/master/test.json");
+        String test = "{\n" +
+                "  \"fruits\":[\n" +
+                "    {\n" +
+                "      \"name\":\"mango\",\n" +
+                "      \"color\":\"yellow\",\n" +
+                "      \"shape\":\"oval\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\":\"appe\",\n" +
+                "      \"color\":\"red\",\n" +
+                "      \"shape\":\"round\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\":\"banana\",\n" +
+                "      \"color\":\"yellow\",\n" +
+                "      \"shape\":\"elongated\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\":\"pineapple\",\n" +
+                "      \"color\":\"yellow\",\n" +
+                "      \"shape\":\"oval\"\n" +
+                "    }\n" +
+                "    ]\n" +
+                "}";
+        test = test.replaceAll("\\s", "");
+        assertThat(json.toString(), is(test));
+    }
+}
---
+++ app/src/main/java/marabillas/loremar/gamehunter/parsers/JSONParser.java
@@ -0,0 +1,42 @@
+package marabillas.loremar.gamehunter.parsers;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * This class is used for parsing JSON documents to extract their data.
+ */
+public class JSONParser {
+    /**
+     * downloads the content from a given url representing a JSON document and converts it into a
+     * java object of a JSONObject class which provides methods to get all of its data.
+     *
+     * @param url url string representing a JSON document
+     * @return an object containg all the data from the JSON document
+     */
+    public JSONObject parse(String url) {
+        try {
+            InputStream inputStream = new URL(url).openStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return new JSONObject(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
