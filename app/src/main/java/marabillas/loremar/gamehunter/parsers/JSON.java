a8877f611dccd087f041bdbaba64f84ef2b37f5e
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-01 00:55:34 +0800

    add and test JSON getters, add JSON_Array, add FailedToGetFieldException

--- app/src/main/java/marabillas/loremar/gamehunter/parsers/JSON.java
+++ app/src/main/java/marabillas/loremar/gamehunter/parsers/JSON.java
@@ -1,18 +1,85 @@
 package marabillas.loremar.gamehunter.parsers;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * This class wraps the JSON data. An instance of this class is created by parsing through
- * JSONParser
+ * This class wraps the JSON data. Use the getter methods to get each field. An instance of this
+ * class is created by parsing through JSONParser.
  */
 public class JSON {
     private JSONObject json;
 
-    // This constructor Should be package private to limit its instantiation to JSONParser
+    // This constructor should be package private to limit its instantiation to JSONParser
     JSON(JSONObject json) {
         this.json = json;
     }
 
-    // todo various getters and putters go here
+    public JSON_Array getArray(String name) throws FailedToGetFieldException {
+        try {
+            JSONArray array = json.getJSONArray(name);
+            return new JSON_Array(array);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public boolean getBoolean(String name) throws FailedToGetFieldException {
+        try {
+            return json.getBoolean(name);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public double getDouble(String name) throws FailedToGetFieldException {
+        try {
+            return json.getDouble(name);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public int getInt(String name) throws FailedToGetFieldException {
+        try {
+            return json.getInt(name);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public long getLong(String name) throws FailedToGetFieldException {
+        try {
+            return json.getLong(name);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public JSON getObject(String name) throws FailedToGetFieldException {
+        try {
+            JSONObject object = json.getJSONObject(name);
+            return new JSON(object);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        } catch (Exception e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    public String getString(String name) throws FailedToGetFieldException {
+        try {
+            return json.getString(name);
+        } catch (JSONException e) {
+            throw new FailedToGetFieldException(e);
+        }
+    }
+
+    /**
+     * @return the whole JSON data as a string
+     */
+    public String toString() {
+        return json.toString();
+    }
 }
--- app/src/test/java/marabillas/loremar/gamehunter/ExampleUnitTest.java
+++ app/src/test/java/marabillas/loremar/gamehunter/ExampleUnitTest.java
@@ -2,7 +2,9 @@ package marabillas.loremar.gamehunter;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,13 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test() {
+        int i = 2;
+        Object object = i;
+        i = (int) object;
+        assertThat(i, is(1));
     }
 }
