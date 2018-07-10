6c36ba3f1a2b92c6893ab57bf91bbd51e8d8e4ad
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-11 02:07:30 +0800

    test getPlatformFilters

---
+++ app/src/androidTest/java/marabillas/loremar/gamehunter/apis/GiantBombTest.java
@@ -0,0 +1,40 @@
+/*
+ *     GameHunter is an Android app for searching video games
+ *     Copyright (C) 2018 Loremar Marabillas
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package marabillas.loremar.gamehunter.apis;
+
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GiantBombTest {
+
+    @Test
+    public void getPlatformFilters() {
+        BaseAPI api = new GiantBomb();
+        Set<String> filters = api.getPlatformFilters();
+        assertThat(filters.size(), is(155));
+        String[] filtersArray = (String[]) filters.toArray();
+        assertThat(filtersArray[0], is("3DO"));
+        assertThat(filtersArray[154], is("ZX Spectrum"));
+    }
+}
