83a264e863a8f1fd25870af4cdf32609241e5882
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-11 01:49:45 +0800

    implement getPlatformFilters for GiantBomb

--- app/src/main/java/marabillas/loremar/gamehunter/apis/GiantBomb.java
+++ app/src/main/java/marabillas/loremar/gamehunter/apis/GiantBomb.java
@@ -21,12 +21,20 @@ package marabillas.loremar.gamehunter.apis;
 
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import marabillas.loremar.gamehunter.GameHunterApp;
 import marabillas.loremar.gamehunter.R;
 import marabillas.loremar.gamehunter.framework.Query;
 import marabillas.loremar.gamehunter.framework.ResultsItem;
+import marabillas.loremar.gamehunter.parsers.FailedToGetFieldException;
+import marabillas.loremar.gamehunter.parsers.FailedToParseException;
+import marabillas.loremar.gamehunter.parsers.json.JSON;
+import marabillas.loremar.gamehunter.parsers.json.JSONParser;
+import marabillas.loremar.gamehunter.parsers.json.JSON_Array;
 
 /**
  *  This class is used for getting access to GiantBomb.com's database using their api.
@@ -34,6 +42,7 @@ import marabillas.loremar.gamehunter.framework.ResultsItem;
 public class GiantBomb extends BaseAPI {
     private final String KEY = GameHunterApp.getInstance().getResources().getString(R.string
             .giantbomb_api_key);
+    private Map<String, Integer> platforms;
 
     @Override
     protected Set<Feature> configure() {
@@ -49,7 +58,45 @@ public class GiantBomb extends BaseAPI {
 
     @Override
     public Set<String> getPlatformFilters() {
-        return null;
+        platforms = new TreeMap<>();
+        Set<String> platformFilters = new TreeSet<>();
+        int page = 0;
+        while (true) {
+            String url = "URL: https://www.giantbomb.com/api/platforms/?api_key=" + KEY +
+                    "&format=json&field_list=name,id&offset=" + (page * 100);
+            JSON json;
+            try {
+                json = new JSONParser().parse(url);
+            } catch (FailedToParseException e) {
+                break;
+            }
+            JSON_Array results;
+            try {
+                results = json.getArray("results");
+                if (results.getCount() <= 0) {
+                    break;
+                }
+                for (int i = 0; i < results.getCount(); ++i) {
+                    String name;
+                    int id;
+                    try {
+                        name = results.getString(i);
+                        id = results.getInt(i);
+                    } catch (FailedToGetFieldException e) {
+                        continue;
+                    }
+                    platforms.put(name, id);
+                    platformFilters.add(name);
+                }
+                if (results.getCount() < 100) {
+                    break;
+                }
+            } catch (FailedToGetFieldException e) {
+                break;
+            }
+            ++page;
+        }
+        return platformFilters;
     }
 
     @Override
