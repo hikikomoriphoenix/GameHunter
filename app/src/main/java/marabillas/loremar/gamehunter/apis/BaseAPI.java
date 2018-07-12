95cf1253cac9160b8e6103fbc9ed32efb69a892b
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-12 16:38:49 +0800

    Add javadoc comments to entries in Feature enum

--- app/src/main/java/marabillas/loremar/gamehunter/apis/BaseAPI.java
+++ app/src/main/java/marabillas/loremar/gamehunter/apis/BaseAPI.java
@@ -31,9 +31,77 @@ import marabillas.loremar.gamehunter.framework.ResultsItem;
  */
 public abstract class BaseAPI {
     protected enum Feature {
-        SEARCH, SEARCH_FILTER, SEARCH_SORT, THUMBNAIL, DESCRIPTION,
-        RELEASE_DATE, FILTER_BY_PLATFORM, FILTER_BY_GENRE, FILTER_BY_THEME, FILTER_BY_YEAR,
-        FILTER_BY_YEARS, SORT_BY, SORT_BY_REVERSIBLE, PAGES, RESULTS_PER_PAGE
+        /**
+         * This feature allows the you to search the game database using a keyword
+         */
+        SEARCH,
+        /**
+         * This feature allows filtering when querying via keyword. SEARCH feature is implied
+         * to be supported, hence, there is no need to include Feature.SEARCH in configuration when
+         * Feature.SEARCH_FILTER is included.
+         */
+        SEARCH_FILTER,
+        /**
+         * This feature allows sorting when querying via keyword. SEARCH feature is implied to
+         * be supported, hence, there is no need to include Feature.SEARCH in configuration when
+         * Feature.SEARCH_SORT is included.
+         */
+        SEARCH_SORT,
+        /**
+         * This feature means the API can provide thumbnails for each game in the results
+         */
+        THUMBNAIL,
+        /**
+         * This feature means the API can provide a brief description of each game in the results
+         */
+        DESCRIPTION,
+        /**
+         * This feature means the API can provide the release date of each game in the results
+         */
+        RELEASE_DATE,
+        /**
+         * This feature means the API can show results based on the platform the game is
+         * developed for.
+         */
+        FILTER_BY_PLATFORM,
+        /**
+         * This feature means the API can show results based on genre
+         */
+        FILTER_BY_GENRE,
+        /**
+         * This feature means the API can show results based on theme
+         */
+        FILTER_BY_THEME,
+        /**
+         * This feature means the API can show results based on the year the game was released
+         */
+        FILTER_BY_YEAR,
+        /**
+         * This feature means the API can show results of video games that are released on
+         * between a set of range of years.
+         */
+        FILTER_BY_YEARS,
+        /**
+         * This feature means the API can show ordered results
+         */
+        SORT_BY,
+        /**
+         * This feature supports ascending and descending options when ordering results. SORT_BY
+         * feature is implied to be supported, hence, there is no need to include Feature.SORT_BY
+         * in configuration when Feature.SORT_BY_REVERSIBLE is included.
+         */
+        SORT_BY_REVERSIBLE,
+        /**
+         * This feature means this API returns a partial amount of the total results as a page.
+         * You have to indicate which page you want to retrieve for partial results.
+         */
+        PAGES,
+        /**
+         * This feature allows you to indicate how many results per page to show. PAGES feature
+         * is implied to be supported, hence, there is no need to include Feature
+         * .RESULTS_PER_PAGE in configuration when Feature.RESULTS_PER_PAGE is included.
+         */
+        RESULTS_PER_PAGE
     }
 
     //private int configuration;
