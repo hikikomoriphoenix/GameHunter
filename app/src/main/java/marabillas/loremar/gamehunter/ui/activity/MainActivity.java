a29b9d006bb7ff084e85f91eea928eb89749fe78
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-20 16:59:21 +0800

    Remove ui prefix from layouts

--- app/src/main/java/marabillas/loremar/gamehunter/ui/activity/MainActivity.java
+++ app/src/main/java/marabillas/loremar/gamehunter/ui/activity/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_activity_main_choose);
+        setContentView(R.layout.activity_main_choose);
 
         GameHunterApp.getInstance().setActivity(this);
     }
